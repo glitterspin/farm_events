c223384af84f811d64bec795df3f29184efad244
Kyle Weisse <kyle.weisse@gmail.com>
2021-08-30 22:27:49 -0400

    pages check

---
+++ pages/posts/do-i-get-it.js
@@ -0,0 +1,8 @@
+export default function DoesItWork() {
+    return <>
+        <h1>I think this should work</h1>
+        <p> Does this work? Ah well, Lorem ipsum 
+        blah blah blah I sure am super profesional,
+        please give me a job I want it so bad.</p>
+    </>
+}
