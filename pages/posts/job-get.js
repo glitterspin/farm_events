53af1cb4ce373bacade6d8448ab80cdc2eb55c16
Kyle Weisse <kyle.weisse@gmail.com>
2021-09-05 11:16:51 -0400

    minor edits to desc

--- pages/posts/job-get.js
+++ pages/posts/job-get.js
@@ -1,7 +1,8 @@
 export default function DoesItWork() {
     return <>
         <h1>I think this should work</h1>
-        <p> Does this work? Ah well, Lorem ipsum 
+        <p> Does this work? Yay it works so far!
+            Lorem ipsum 
         blah blah blah I sure am super profesional,
         please give me a job I want it so bad.</p>
     </>
