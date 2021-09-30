bfa9a7900c0802e9771b8ff50cd46191de1e17a3
Kyle Weisse <kyle.weisse@gmail.com>
2021-09-30 09:13:20 -0400

    minor edits

--- nextjs-blog/pages/posts/job-get.js
+++ nextjs-blog/pages/posts/job-get.js
@@ -3,7 +3,7 @@ export default function DoesItWork() {
         <h1>I think this should work</h1>
         <p> Does this work? Yay it works so far!
             Lorem ipsum 
-        blah blah blah I sure am super profesional,
+        I sure am super profesional,
         I would be a great asset to your workplace.</p>
     </>
 }
