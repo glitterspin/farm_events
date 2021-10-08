bc924e5baad4d1882e226a8535e6420e8d86fed2
Kyle Weisse <kyle.weisse@gmail.com>
2021-10-08 10:28:20 -0400

    testing routing behavior

--- nextjs-blog/pages/posts/job-get.js
+++ nextjs-blog/pages/posts/job-get.js
@@ -1,9 +1,8 @@
 export default function DoesItWork() {
     return <>
         <h1>I think this should work</h1>
-        <p> Does this work? Yay it works so far!
-            Lorem ipsum 
-        I am super reliable, agreeable, and funny.
-        I would be a great asset to your workplace.</p>
+        <p> Testing to make sure I understand how this works, so this should be reachable @local:3000/posts/job-get. 
+            Let's see if it works!
+            Awesome it works. No formatting yet but whatever it's basic tutorial.</p>
     </>
 }
