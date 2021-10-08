b8a9c62d4fb781176ce8d622e93e6188d425996d
Kyle Weisse <kyle.weisse@gmail.com>
2021-10-08 10:33:04 -0400

    first post edited to include routing

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,7 +1,14 @@
 import Link from 'next/link'
-import Head from 'next/head'
-// import Layout from '../../../components/layout'
 
 export default function FirstPost() {
-    return <h1> First Postagain </h1>
+    return (
+        <>
+        <h1>First Post yet again. Third post?</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to home?</a>
+            </Link>
+        </h2>
+        </>
+    )
 }
